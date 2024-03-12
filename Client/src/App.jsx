51863b1a2bc50c9052baa7f81d7956fdb5214363
Selani00 <pkselani00@gmail.com>
2024-03-12 23:41:46 +0530

    install flowbit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
 import Project from './Pages/Projects'
 import Dashboard from './Pages/Dashboard'
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>}/>
