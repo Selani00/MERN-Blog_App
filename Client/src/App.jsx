e9924076f8b7cafd90bdabc881675800f255416b
Selani00 <pkselani00@gmail.com>
2024-03-12 23:21:40 +0530

    Install routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Project from './Pages/Projects'
+import Dashboard from './Pages/Dashboard'
 
 const App = () => {
   return (
-    <div>
-      App
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/project" element={<Project/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+
+      </Routes>
       
-    </div>
+    </BrowserRouter>
   )
 }
 
