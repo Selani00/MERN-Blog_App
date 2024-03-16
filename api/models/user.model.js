6f869f97ec10034a9774e9e32fd93367f5073f95
Selani00 <pkselani00@gmail.com>
2024-03-16 15:00:56 +0530

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.userSchema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password :{
+        type: String,
+        required: true,
+    },
+
+},{timestampe:true})
+// need to add time of creation and time of update . That's why we use timestampe:true . So that it will automatically add time of creation and time of update.
+
+// create a model. Name is User . mongodb automatically create a collection with the name of users.
+const User = mongoose.model("User",userSchema);
+
+export default User;
+
