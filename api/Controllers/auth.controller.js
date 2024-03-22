4d1203ee2f12def709514ceb1f62b604d918d68f
Selani00 <pkselani00@gmail.com>
2024-03-22 22:52:55 +0530

    fix the routing

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import  {errorHandler}  from "../utilts/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
@@ -32,3 +33,37 @@ export const signup = async (req, res,next) => {
   // to save the user in the database
   
 };
+
+export const signin = async (req, res,next) => {
+  const {email,password}= res.body;
+
+  if (!email || !password || email === " " || password === " ") {
+    next(errorHandler(400, "All fields are required!!" ));
+  }
+
+  try{
+
+    const validuser= await User.findOne({email});
+
+
+    if(!validuser || !validPassword){
+      next(errorHandler(400, "User not found" ));
+    }
+
+    const validPassword= bcryptjs.compareSync(password,validuser.password);
+
+    if(!validPassword){
+      next(errorHandler(400, "Invalid password" ));
+    }
+
+    const token =jwt.sign(
+      {id : validuser._id, }, 
+      // secret key is unique key for that is only for you.
+      process.env.JWT_SECRET,
+      res.status(200).cookie('access_token',token,{httpOnly:true}).json(validuser)
+      );
+
+  }catch(err){
+    next(err);
+  }
+}
