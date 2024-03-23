4d234d380a13d924a2690d95a83a8cf920ee8241
Selani00 <pkselani00@gmail.com>
2024-03-23 17:41:58 +0530

    Update header component with user details

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const path = useLocation().pathname;
+
+  // to get the current user
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
     <Navbar className="border-b-2">
@@ -36,23 +40,50 @@ const Header = () => {
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/signin">
-          <Button className="" gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown 
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar 
+            alt='user'
+            img={currentUser.profilePicture}
+            rounded
+            />
+          }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+              <Link to={'/dashboard?tab=profile'}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+                
+              </Link>
+              <Dropdown.Divider/>
+              
+              <Dropdown.Item>Log Out</Dropdown.Item>
+            </Dropdown.Header>
+          </Dropdown>
+        ) : (
+          <Link to="/signin">
+            <Button className="" gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
         <Navbar.Toggle />
       </div>
 
       <Navbar.Collapse>
         {/* with out as={'div'} it will get error . cz we difine path in two places */}
-        <Navbar.Link active={path === "/"} as={'div'}>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={'div'}>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/project"} as={'div'}>
+        <Navbar.Link active={path === "/project"} as={"div"}>
           <Link to="/project">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
